7dbc6527e1eed4a84c3caacb5eba301b0de83b5a
34Maryna <aupairtickets@gmail.com>
2023-03-31 23:20:00 +0300

    done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import throttle from 'lodash.throttle';
+import player from '@vimeo/player';
 
+const VCT_KEY = 'videoplayer-current-time';
+
+const onPlay = function(event) {
+localStorage.setItem(VCT_KEY, event.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+messageOut();
+
+function messageOut() {
+  const savedTime = localStorage.getItem(VCT_KEY);
+  if (savedTime) {
+    player.setCurrentTime(savedTime);
+  }
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('[name="email"]'),
+  textarea: document.querySelector('[name="message"]'),
+  };
+
+const STORAGE_KEY = 'feedback-form-state';
+
+refs.form.addEventListener('input', throttle(onForm, 500));
+refs.form.addEventListener('submit', onSubmit);
+
+localStorageSetting();
+
+function onSubmit(evt) {
+  evt.preventDefault();
+  const currentValue = localStorage.getItem(STORAGE_KEY);
+  const parseValue = JSON.parse(currentValue);
+  evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function onForm(evt) {
+  const email = refs.input.value;
+  const messageValue = refs.textarea.value;
+  const message = messageValue.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify({ email, message }));
+}
+
+function localStorageSetting() {
+  const currentValue = localStorage.getItem(STORAGE_KEY);
+  const parseValue = JSON.parse(currentValue);
+
+  if (currentValue) {
+    refs.input.value = parseValue.email;
+    refs.textarea.value = parseValue.message;
+  }
+}
